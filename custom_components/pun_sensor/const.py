558d94d2dea012cb8aab9be7343431682f6faccb
virtualdj <virtualdj@users.noreply.github.com>
2022-11-05 20:30:52 +0100

    Rinomina fasce

--- custom_components/pun_sensor/const.py
+++ custom_components/pun_sensor/const.py
@@ -1,1 +1,8 @@
+# Dominio HomeAssistant
 DOMAIN = "pun_sensor"
+
+# Tipi di sensore da creare
+PUN_FASCIA_MONO = 0
+PUN_FASCIA_F1 = 1
+PUN_FASCIA_F2 = 2
+PUN_FASCIA_F3 = 3
--- custom_components/pun_sensor/sensor.py
+++ custom_components/pun_sensor/sensor.py
@@ -1,59 +1,91 @@
 
 from homeassistant.components.sensor import (
-    SensorDeviceClass,
     SensorEntity,
-    SensorStateClass,
+    SensorStateClass
 )
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, VOLUME_LITERS
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import DiscoveryInfoType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity, DataUpdateCoordinator
-import logging
 
 from . import PUNDataHub
-from .const import DOMAIN
+from .const import DOMAIN, PUN_FASCIA_MONO, PUN_FASCIA_F1, PUN_FASCIA_F2, PUN_FASCIA_F3
 
+import logging
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(
-    hass: HomeAssistant,
-    config: ConfigEntry,
+async def async_setup_platform(hass: HomeAssistant, config: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
-) -> None:
-    """Set up the sensor platform."""
+    discovery_info: DiscoveryInfoType | None = None) -> None:
+    """Inizializza e crea i sensori"""
     _LOGGER.debug("Sensors async setup")
+
+    # Restituisce il coordinator
     coordinator = hass.data[DOMAIN]["coordinator"]
-    #devices = await coordinator.hub.list_devices()
 
-    #entities = []
-
-    #for device in devices:
-    #    entities.append(PUNSensorEntity(coordinator, device))   
-
+    # Aggiunge i sensori (legati al coordinator)
     entities = []
-    entities.append(PUNSensorEntity(coordinator, 'F1'))
-    entities.append(PUNSensorEntity(coordinator, 'F2'))
-    entities.append(PUNSensorEntity(coordinator, 'F3'))
+    entities.append(PUNSensorEntity(coordinator, PUN_FASCIA_MONO))
+    entities.append(PUNSensorEntity(coordinator, PUN_FASCIA_F1))
+    entities.append(PUNSensorEntity(coordinator, PUN_FASCIA_F2))
+    entities.append(PUNSensorEntity(coordinator, PUN_FASCIA_F3))
     async_add_entities(entities, update_before_add=True)
 
 
 class PUNSensorEntity(CoordinatorEntity, SensorEntity):
     hub: PUNDataHub
-    name: str
+    tipo: int
 
-    def __init__(self, coordinator: DataUpdateCoordinator, name: str) -> None:
+    def __init__(self, coordinator: DataUpdateCoordinator, tipo: int) -> None:
         super().__init__(coordinator)
-        _LOGGER.debug("PUNSensor created (" + name + ")")
+
+        # Inizializza coordinator e tipo
         self.hub = coordinator.hub
+        self.tipo = tipo
+
+        # Inizializza le proprietà comuni
         self._state = 0
-        self._attr_name = "Real PUN " + name
-        self._attr_native_unit_of_measurement = "€/kWh"
         self._attr_state_class = SensorStateClass.MEASUREMENT
 
     @property
     def state(self):
         return self.hub.nane
+
+    @property
+    def unit_of_measurement(self):
+        """Unita' di misura"""
+        return "€/kWh"
+
+    @property
+    def icon(self):
+        """Icona da usare nel frontend"""
+        return "mdi:chart-line"
+
+    @property
+    def entity_id(self):
+        """Restituisce l'entity id del sensore"""
+        if (self.tipo == PUN_FASCIA_F3):
+            return DOMAIN + ".fascia_f3"
+        elif (self.tipo == PUN_FASCIA_F2):
+            return DOMAIN + ".fascia_f2"
+        elif (self.tipo == PUN_FASCIA_F1):
+            return DOMAIN + ".fascia_f1"
+        elif (self.tipo == PUN_FASCIA_MONO):
+            return DOMAIN + ".mono_orario"
+        else:
+            return None
+
+    @property
+    def name(self):
+        """Restituisce il nome del sensore"""
+        if (self.tipo == PUN_FASCIA_F3):
+            return "PUN fascia F3"
+        elif (self.tipo == PUN_FASCIA_F2):
+            return "PUN fascia F2"
+        elif (self.tipo == PUN_FASCIA_F1):
+            return "PUN fascia F1"
+        elif (self.tipo == PUN_FASCIA_MONO):
+            return "PUN mono-orario"
+        else:
+            return None
