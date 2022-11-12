4d277501c8967429ef6b26d03567a6f4f5547692
virtualdj <virtualdj@users.noreply.github.com>
2022-11-12 14:45:02 +0100

    Download dal sito funzionante

--- custom_components/pun_sensor/__init__.py
+++ custom_components/pun_sensor/__init__.py
@@ -1,5 +1,11 @@
 """Prezzi mensili PUN"""
-from datetime import timedelta
+from datetime import date, timedelta, datetime
+import holidays
+from statistics import mean
+import zipfile, io
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as et
+
 from aiohttp import ClientSession
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
@@ -7,8 +13,7 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.helpers.discovery import async_load_platform
 
-from .const import DOMAIN
-import random #Temporary
+from .const import DOMAIN, PUN_FASCIA_MONO, PUN_FASCIA_F1, PUN_FASCIA_F2, PUN_FASCIA_F3
 
 import logging
 _LOGGER = logging.getLogger(__name__)
@@ -28,13 +33,12 @@ async def async_setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
     }
     hass.async_create_task(async_load_platform(hass, "sensor", DOMAIN, {}, config))
     return True
-    
 
 class PUNDataUpdateCoordinator(DataUpdateCoordinator):
     session: ClientSession
 
-    """ Gestione dell'aggiornamento da Home Assistant """
     def __init__(self, hass: HomeAssistant, config: ConfigEntry) -> None:
+        """ Gestione dell'aggiornamento da Home Assistant """
         super().__init__(
             hass,
             _LOGGER,
@@ -50,22 +54,149 @@ class PUNDataUpdateCoordinator(DataUpdateCoordinator):
         self.config = config
 
         # Inizializza i valori
+        self.last_successful_update = datetime.min
         self.pun = [0.0, 0.0, 0.0, 0.0]
         self.orari = [0, 0, 0, 0]
-        # TODO: Provare a inventare questi valori e leggerli dall'Entity tramite l'indice (tipo)
-        _LOGGER.debug("Coordinator initialized")
-   
+        _LOGGER.debug("Coordinator inizializzato.")
+  
     async def _async_update_data(self):
-        _LOGGER.debug("Coordinator update requested")
-        self.orari[0] = random.randint(0, 2)
-        self.orari[1] = 1
-        self.orari[2] = random.randint(0, 2)
-        self.orari[3] = 1
-        for x in range(4):
-            self.pun[x] = random.randint(20, 50) / 2
+        """Aggiornamento dati a intervalli prestabiliti"""
 
-        _LOGGER.debug('Valori PUN: ' + ', '.join(str(e) for e in self.pun))
-        #async with async_timeout.timeout(10):
-        #    return await self.my_api.fetch_data()
+        # Verifica che sia passato il tempo minimo dall'ultimo controllo
+        if (datetime.now() - self.last_successful_update).total_seconds() < 60:
+            _LOGGER.debug("Aggiornamento dati non necessario (già eseguito).")
+            return
+        
+        # Calcola l'intervallo di date per il mese corrente
+        date_today = date.today()
+        date_start = date(date_today.year, date_today.month, 1)
+
+        # URL del sito Mercato elettrico
+        LOGIN_URL = 'https://www.mercatoelettrico.org/It/Tools/Accessodati.aspx?ReturnUrl=%2fIt%2fdownload%2fDownloadDati.aspx%3fval%3dMGP_Prezzi&val=MGP_Prezzi'
+        DOWNLOAD_URL = 'https://www.mercatoelettrico.org/It/download/DownloadDati.aspx?val=MGP_Prezzi'
+        
+        # Apre la pagina per generare i cookie e i campi nascosti
+        async with self.session.get(LOGIN_URL) as response:
+            soup = BeautifulSoup(await response.read(), features="html.parser")
+        
+        # Recupera i campi nascosti __VIEWSTATE e __EVENTVALIDATION per la prossima richiesta
+        viewstate = soup.find("input",{"name":"__VIEWSTATE"})['value']
+        eventvalidation = soup.find("input",{"name":"__EVENTVALIDATION"})['value']
+        login_payload = {
+            'ctl00$ContentPlaceHolder1$CBAccetto1': 'on',
+            'ctl00$ContentPlaceHolder1$CBAccetto2': 'on',
+            'ctl00$ContentPlaceHolder1$Button1': 'Accetto',
+            '__VIEWSTATE': viewstate,
+            '__EVENTVALIDATION': eventvalidation
+        }
+
+        # Effettua il login (che se corretto porta alla pagina di download XML grazie al 'ReturnUrl')
+        async with self.session.post(LOGIN_URL, data=login_payload) as response:
+            soup = BeautifulSoup(await response.read(), features="html.parser")
+
+        # Recupera i campi nascosti __VIEWSTATE per la prossima richiesta
+        viewstate = soup.find("input",{"name":"__VIEWSTATE"})['value']    
+        data_request_payload = {
+            'ctl00$ContentPlaceHolder1$tbDataStart': date_start.strftime('%d/%m/%Y'),
+            'ctl00$ContentPlaceHolder1$tbDataStop': date_today.strftime('%d/%m/%Y'),
+            'ctl00$ContentPlaceHolder1$btnScarica': 'scarica+file+xml+compresso',
+            '__VIEWSTATE': viewstate
+        }
+
+        # Effettua il download dello ZIP con i file XML
+        async with self.session.post(DOWNLOAD_URL, data=data_request_payload) as response:
+            # Scompatta lo ZIP in memoria
+            try:
+                archive = zipfile.ZipFile(io.BytesIO(await response.read()))
+            except:
+                # Esce perché l'output non è uno ZIP
+                raise SystemExit('ERRORE: errore durante il download dei dati dal sito.')
+
+        _LOGGER.debug('File trovati nell\'archivio: ' + ', '.join(str(fn) for fn in archive.namelist()))
+
+        # Carica le festività
+        it_holidays = holidays.IT()
+
+        # Inizializza le variabili di conteggio dei risultati
+        mono = []
+        f1 = []
+        f2 = []
+        f3 = []
+
+        # Esamina ogni file XML nello ZIP (ordinandoli prima)
+        for fn in sorted(archive.namelist()):
+            # Scompatta il file XML in memoria
+            xml_tree = et.parse(archive.open(fn))
+
+            # Parsing dell'XML (1 file = 1 giorno)
+            xml_root = xml_tree.getroot()
+
+            # Estrae la data dal primo elemento (sarà identica per gli altri)
+            dat_string = xml_root.find('Prezzi').find('Data').text #YYYYMMDD
+
+            # Converte la stringa giorno in data
+            dat_date = date(int(dat_string[0:4]), int(dat_string[4:6]), int(dat_string[6:8]))
+
+            # Verifica la festività
+            festivo = dat_date in it_holidays
+
+            # Estrae le rimanenti informazioni
+            for prezzi in xml_root.iter('Prezzi'):
+                # Estrae l'ora dall'XML
+                ora = int(prezzi.find('Ora').text) - 1 # 1..24
+                
+                # Estrae il prezzo PUN dall'XML in un float
+                prezzo_string = prezzi.find('PUN').text
+                prezzo_string = prezzo_string.replace('.','').replace(',','.')
+                prezzo = float(prezzo_string) / 1000
+
+                # Estrae la fascia oraria
+                fascia = get_fascia(dat_date, festivo, ora)
+
+                # Calcola le statistiche
+                mono.append(prezzo)
+                if fascia == 3:
+                    f3.append(prezzo)
+                elif fascia == 2:
+                    f2.append(prezzo)
+                elif fascia == 1:
+                    f1.append(prezzo)
+
+        # Salva i risultati nel coordinator
+        self.orari[PUN_FASCIA_MONO] = len(mono)
+        self.orari[PUN_FASCIA_F1] = len(f1)
+        self.orari[PUN_FASCIA_F2] = len(f2)
+        self.orari[PUN_FASCIA_F3] = len(f3)
+        if self.orari[PUN_FASCIA_MONO] > 0:
+            self.pun[PUN_FASCIA_MONO] = mean(mono)
+        if self.orari[PUN_FASCIA_F1] > 0:
+            self.pun[PUN_FASCIA_F1] = mean(f1)
+        if self.orari[PUN_FASCIA_F2] > 0:
+            self.pun[PUN_FASCIA_F2] = mean(f2)
+        if self.orari[PUN_FASCIA_F3] > 0:
+            self.pun[PUN_FASCIA_F3] = mean(f3)
+        self.last_successful_update = datetime.now()
+
         return
 
+def get_fascia(data, festivo, ora):
+    """Restituisce il numero di fascia oraria"""
+    #F1 = lu-ve 8-19
+    #F2 = lu-ve 7-8, lu-ve 19-23, sa 7-23
+    #F3 = lu-sa 0-7, lu-sa 23-24, do, festivi
+    if festivo or (data.weekday() == 6):
+        # Festivi e domeniche
+        return 3
+    elif (data.weekday() == 5):
+        # Sabato
+        if (ora >= 7) and (ora < 23):
+            return 2
+        else:
+            return 3
+    else:
+        # Altri giorni della settimana
+        if (ora == 7) or ((ora >= 19) and (ora < 23)):
+            return 2
+        elif (ora == 23) or ((ora >= 0) and (ora < 7)):
+            return 3
+    return 1
