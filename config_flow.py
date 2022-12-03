d0224d7ac626867440105555882733f9fd954115
virtualdj <virtualdj@users.noreply.github.com>
2022-12-03 16:48:18 +0100

    Opzioni caricate all'avvio ma non applicate

--- config_flow.py
+++ config_flow.py
@@ -1,4 +1,6 @@
 from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.data_entry_flow import FlowResult
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from .const import (
@@ -11,11 +13,53 @@ from .const import (
 import logging
 _LOGGER = logging.getLogger(__name__)
 
+class PUNOptionsFlow(config_entries.OptionsFlow):
+    """Opzioni per prezzi PUN"""
+
+    def __init__(self, entry: config_entries.ConfigEntry) -> None:
+        """Inizializzazione options flow"""
+        self.config_entry = entry
+
+    async def async_step_init(self, user_input=None) -> FlowResult:
+        """Gestisce le opzioni"""
+        errors = {}
+        if user_input is not None:
+            # Validate user input
+            if user_input[CONF_SCAN_HOUR] > 10:
+                # See next section on create entry usage
+                return self.async_create_entry(
+                    title="PUN",
+                    data=user_input
+                )
+
+            #errors["base"] = "auth_error"
+            errors[CONF_SCAN_HOUR] = "invalid_auth"
+
+        # Schema dati richiesti
+        data_schema = {
+            vol.Required(CONF_SCAN_HOUR, default=self.config_entry.data[CONF_SCAN_HOUR]): vol.All(cv.positive_int, vol.Range(min=0, max=23)),
+            vol.Optional(CONF_SCAN_INTERVAL, default=self.config_entry.data[CONF_SCAN_INTERVAL]): vol.All(cv.positive_int, vol.Range(min=10)),
+            vol.Optional(CONF_ACTUAL_DATA_ONLY, default=self.config_entry.data[CONF_ACTUAL_DATA_ONLY]): cv.boolean,
+        }
+
+        return self.async_show_form(
+            step_id="init", data_schema=vol.Schema(data_schema), errors=errors
+        )
+
+    
 class PUNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow per prezzi PUN"""
+    """Configurazione per prezzi PUN"""
 
     # Versione della configurazione (per utilizzi futuri)
     VERSION = 1
+
+    # TODO: Non deve essere eseguita più volte!
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(entry: config_entries.ConfigEntry) -> PUNOptionsFlow:
+        """Ottiene le opzioni per questa configurazione"""
+        return PUNOptionsFlow(entry)
 
     async def async_step_user(self, user_input=None):
         """Gestione prima configurazione da Home Assistant"""
@@ -43,3 +87,4 @@ class PUNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=vol.Schema(data_schema), errors=errors
         )
+
