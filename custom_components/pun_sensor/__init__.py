bc9f7862b416e2bd2bcb77948b5cdc997923f7b0
virtualdj <virtualdj@users.noreply.github.com>
2022-12-10 15:37:50 +0100

    Evita di aggiornare 2 volte all'avvio

--- custom_components/pun_sensor/__init__.py
+++ custom_components/pun_sensor/__init__.py
@@ -46,8 +46,6 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry) -> bool:
     # Registra il callback di modifica opzioni
     config.async_on_unload(config.add_update_listener(update_listener))
 
-    # Attende il primo refresh
-    await coordinator.async_config_entry_first_refresh()
     return True
 
 
