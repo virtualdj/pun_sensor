cfe539f75bfef07a092cdf855c7b547763a71064
0x4b1d <ilsecco94@gmail.com>
2024-06-15 15:56:37 +0000

    Fix linter

--- custom_components/pun_sensor/__init__.py
+++ custom_components/pun_sensor/__init__.py
@@ -35,11 +35,10 @@ PLATFORMS: list[str] = ["sensor"]
 async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry) -> bool:
     """Impostazione dell'integrazione da configurazione Home Assistant."""
 
-
     # Carica le dipendenze di holidays in background per evitare errori nel log
-    if (AwesomeVersion(HA_VERSION) >= AwesomeVersion("2024.5.0")):
+    if AwesomeVersion(HA_VERSION) >= AwesomeVersion("2024.5.0"):
         with async_pause_setup(hass, SetupPhases.WAIT_IMPORT_PACKAGES):
-            await hass.async_add_import_executor_job(holidays.IT)
+            await hass.async_add_import_executor_job(holidays.IT)  # type: ignore
 
     # Salva il coordinator nella configurazione
     coordinator = PUNDataUpdateCoordinator(hass, config)
